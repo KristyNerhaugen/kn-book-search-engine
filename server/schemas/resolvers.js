dfac3e90c8f6aec6191a6928389f545e217f6759
kristynerhaugen <kristynerhaugen@gmail.com>
2022-09-21 13:04:49 -0500

    add user, login, save book, and remove book mutations added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,4 +14,64 @@ const resolvers = {
       throw new AuthenticationError("User is not logged in");
     },
   },
+
+  Mutation: {
+    // add user mutation
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // log in mutation
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect login credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect login credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    // saveBook mutation
+    saveBook: async (parent, { bookInfo }, context) => {
+      if (context.user) {
+        const savedBook = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookInfo } },
+          { new: true, runValidators: true }
+        );
+
+        return savedBook;
+      }
+
+      throw new AuthenticationError(
+        "You need to be logged in to save a new book."
+      );
+    },
+    // removeBook mutation
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const removeBook = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } }
+        );
+
+        return removeBook;
+      }
+
+      throw new AuthenticationError(
+        "You need to be logged in to remove a book."
+      );
+    },
+  },
 };
+
+module.exports = resolvers;
